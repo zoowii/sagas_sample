5051c44050300f555049517b2c21d2d16b774eb2
zoowii <zoowii@outlook.com>
2020-09-03 22:13:55 +0800

    saga server read db url from environment

--- saga_server/server.go
+++ saga_server/server.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	pb "github.com/zoowii/saga_server/api"
 	"github.com/zoowii/saga_server/app"
 	services "github.com/zoowii/saga_server/services"
@@ -8,12 +9,21 @@ import (
 	"log"
 	"net"
 	_ "net/http/pprof"
+	"os"
 )
 
 const (
 	address = ":9009"
 	network = "tcp"
 )
+
+func getDbUrl() (string, error) {
+	dbUrl := os.Getenv("DATABASE_URL")
+	if len(dbUrl) > 0 {
+		return dbUrl, nil
+	}
+	return "", errors.New("DATABASE_URL not set")
+}
 
 func main() {
 	listener, err := net.Listen(network, address)
@@ -24,8 +34,15 @@ func main() {
 	log.Println(address + " net.Listing...")
 	grpcServer := grpc.NewServer()
 
-	// TODO: load config from config file or environment
-	dbUrl := "root:123456@tcp(127.0.0.1)/saga_server?charset=utf8&checkConnLiveness=true&parseTime=true"
+	testDbUrl := "root:123456@tcp(127.0.0.1)/saga_server?charset=utf8&checkConnLiveness=true&parseTime=true"
+
+	// load config from config file or environment
+	dbUrl, err := getDbUrl()
+	if err != nil {
+		log.Printf("get db url err: %v\n", err.Error())
+		log.Println("use default test db url")
+		dbUrl = testDbUrl
+	}
 
 	sagaApp, err := app.NewApplicationContext(app.SetDbUrl(dbUrl))
 	if err != nil {
@@ -33,7 +50,7 @@ func main() {
 		return
 	}
 	defer sagaApp.Close()
-	sagaServerService, err  := services.NewSagaServerService(sagaApp)
+	sagaServerService, err := services.NewSagaServerService(sagaApp)
 	if err != nil {
 		log.Fatalf("saga server service err: %v", err)
 		return
